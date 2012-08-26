67d859b7357f3a0b44143151303ea97d669bbc56
NSParisi@gmail.com <NSParisi@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2012-08-26 05:47:40 +0000

    adding damage component

---
+++ CyborgPunch/CyborgPunch/CyborgPunch/Game/Limbs/Damage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CyborgPunch.Core;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using CyborgPunch.Game.Enemies;
+
+namespace CyborgPunch.Game.Limbs
+{
+    public class Damage : Component
+    {
+        public int damageValue;
+
+        public Damage(int damage)
+            : base()
+        {
+            damageValue = damage;
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            List<Enemy> enemies = EnemyManager.Instance.GetEnemies();
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                if (enemies[i].blob.Collides(this.blob))
+                {
+                    //hit enemy
+                    enemies[i].Hit(this);
+                }
+            }
+        }
+    }
+}
