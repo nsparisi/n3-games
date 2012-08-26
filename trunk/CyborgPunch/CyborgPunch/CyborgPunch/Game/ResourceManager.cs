bed356c650b067986b482eb4f53cc0f670f2fc87
NSParisi@gmail.com <NSParisi@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2012-08-26 05:13:03 +0000

    new assets

--- trunk/CyborgPunch/CyborgPunch/CyborgPunch/Game/ResourceManager.cs
+++ trunk/CyborgPunch/CyborgPunch/CyborgPunch/Game/ResourceManager.cs
@@ -10,129 +10,153 @@ namespace CyborgPunch.Game
     public class ResourceManager
     {
 
-        public static List<Texture2D> sheetHeads;
-        public static List<Texture2D> sheetLeftArms;
-        public static List<Texture2D> sheetRightArms;
-        public static List<Texture2D> sheetLeftLegs;
-        public static List<Texture2D> sheetRightLegs;
-        public static List<Texture2D> sheetTorsos;
+        public static Texture2D TEXTURE_HUMAN_UP_HEAD;
+        public static Texture2D TEXTURE_HUMAN_UP_TORSO;
+        public static Texture2D TEXTURE_HUMAN_UP_LEFTARM;
+        public static Texture2D TEXTURE_HUMAN_UP_RIGHTARM;
+        public static Texture2D TEXTURE_HUMAN_UP_LEFTLEG;
+        public static Texture2D TEXTURE_HUMAN_UP_RIGHTLEG;
 
-        public static List<Texture2D> spriteHeads;
-        public static List<Texture2D> spriteLeftArms;
-        public static List<Texture2D> spriteRightArms;
-        public static List<Texture2D> spriteLeftLegs;
-        public static List<Texture2D> spriteRightLegs;
-        public static List<Texture2D> spriteTorsos;
 
-        public static Texture2D GetSpritePart(LimbType limb, PartType type)
-        {
-            if (limb == LimbType.Head)
-            {
-                return spriteHeads[(int)type];
-            }
-            else if (limb == LimbType.Torso)
-            {
-                return spriteTorsos[(int)type];
-            }
-            else if (limb == LimbType.LeftArm)
-            {
-                return spriteLeftArms[(int)type];
-            }
-            else if (limb == LimbType.LeftLeg)
-            {
-                return spriteLeftLegs[(int)type];
-            }
-            else if (limb == LimbType.RightArm)
-            {
-                return spriteRightArms[(int)type];
-            }
-            else if (limb == LimbType.RightLeg)
-            {
-                return spriteRightLegs[(int)type];
-            }
+        public static Texture2D TEXTURE_HUMAN_DOWN_HEAD;
+        public static Texture2D TEXTURE_HUMAN_DOWN_TORSO;
+        public static Texture2D TEXTURE_HUMAN_DOWN_LEFTARM;
+        public static Texture2D TEXTURE_HUMAN_DOWN_RIGHTARM;
+        public static Texture2D TEXTURE_HUMAN_DOWN_LEFTLEG;
+        public static Texture2D TEXTURE_HUMAN_DOWN_RIGHTLEG;
 
-            return null;
-        }
+        public static Texture2D TEXTURE_HUMAN_LEFT_HEAD;
+        public static Texture2D TEXTURE_HUMAN_LEFT_TORSO;
+        public static Texture2D TEXTURE_HUMAN_LEFT_LEFTARM;
+        public static Texture2D TEXTURE_HUMAN_LEFT_LEFTLEG;
 
-        public static Texture2D GetSheetPart(LimbType limb, PartType type)
-        {
-            if (limb == LimbType.Head)
-            {
-                return sheetHeads[(int)type];
-            }
-            else if (limb == LimbType.Torso)
-            {
-                return sheetTorsos[(int)type];
-            }
-            else if (limb == LimbType.LeftArm)
-            {
-                return sheetLeftArms[(int)type];
-            }
-            else if (limb == LimbType.LeftLeg)
-            {
-                return sheetLeftLegs[(int)type];
-            }
-            else if (limb == LimbType.RightArm)
-            {
-                return sheetRightArms[(int)type];
-            }
-            else if (limb == LimbType.RightLeg)
-            {
-                return sheetRightLegs[(int)type];
-            }
+        public static Texture2D TEXTURE_HUMAN_RIGHT_HEAD;
+        public static Texture2D TEXTURE_HUMAN_RIGHT_TORSO;
+        public static Texture2D TEXTURE_HUMAN_RIGHT_RIGHTARM;
+        public static Texture2D TEXTURE_HUMAN_RIGHT_RIGHTLEG;
 
-            return null;
-        }
+
+        /* ROBOT PARTS */
+
+        /*  Bite Head */
+        public static Texture2D TEXTURE_ROBOT_BITEHEAD_UP;
+        public static Texture2D TEXTURE_ROBOT_BITEHEAD_DOWN;
+        public static Texture2D TEXTURE_ROBOT_BITEHEAD_LEFT;
+        public static Texture2D TEXTURE_ROBOT_BITEHEAD_RIGHT;
+
+        /* Laser Head */
+        public static Texture2D TEXTURE_ROBOT_LASERHEAD_UP;
+        public static Texture2D TEXTURE_ROBOT_LASERHEAD_DOWN;
+        public static Texture2D TEXTURE_ROBOT_LASERHEAD_LEFT;
+        public static Texture2D TEXTURE_ROBOT_LASERHEAD_RIGHT;
+
+        /* Bomb Head */
+        public static Texture2D TEXTURE_ROBOT_BOMBHEAD_UP;
+        public static Texture2D TEXTURE_ROBOT_BOMBHEAD_DOWN;
+        public static Texture2D TEXTURE_ROBOT_BOMBHEAD_LEFT;
+        public static Texture2D TEXTURE_ROBOT_BOMBHEAD_RIGHT;
+
+
+        /* Torso */
+        public static Texture2D TEXTURE_ROBOT_TORSO_UP;
+        public static Texture2D TEXTURE_ROBOT_TORSO_DOWN;
+        public static Texture2D TEXTURE_ROBOT_TORSO_LEFT;
+        public static Texture2D TEXTURE_ROBOT_TORSO_RIGHT;
+
+
+        /* Long Arm */
+        public static Texture2D TEXTURE_ROBOT_LONGARM_LEFTARM_UP;
+        public static Texture2D TEXTURE_ROBOT_LONGARM_RIGHTARM_UP;
+
+        public static Texture2D TEXTURE_ROBOT_LONGARM_LEFTARM_DOWN;
+        public static Texture2D TEXTURE_ROBOT_LONGARM_RIGHTARM_DOWN;
+
+        public static Texture2D TEXTURE_ROBOT_LONGARM_LEFTARM_LEFT;
+        
+        public static Texture2D TEXTURE_ROBOT_LONGARM_RIGHTARM_RIGHT;
+
+
+        /* Hammer Arm */
+        public static Texture2D TEXTURE_ROBOT_HAMMERARM_LEFTARM_UP;
+        public static Texture2D TEXTURE_ROBOT_HAMMERARM_RIGHTARM_UP;
+
+        public static Texture2D TEXTURE_ROBOT_HAMMERARM_LEFTARM_DOWN;
+        public static Texture2D TEXTURE_ROBOT_HAMMERARM_RIGHTARM_DOWN;
+
+        public static Texture2D TEXTURE_ROBOT_HAMMERARM_LEFTARM_LEFT;
+
+        public static Texture2D TEXTURE_ROBOT_HAMMERARM_RIGHTARM_RIGHT;
+
+
+        /* Hook Arm */
+
+        public static Texture2D TEXTURE_ROBOT_HOOKARM_LEFTARM_UP;
+        public static Texture2D TEXTURE_ROBOT_HOOKARM_RIGHTARM_UP;
+
+        public static Texture2D TEXTURE_ROBOT_HOOKARM_LEFTARM_DOWN;
+        public static Texture2D TEXTURE_ROBOT_HOOKARM_RIGHTARM_DOWN;
+
+        public static Texture2D TEXTURE_ROBOT_HOOKARM_LEFTARM_LEFT;
+
+        public static Texture2D TEXTURE_ROBOT_HOOKARM_RIGHTARM_RIGHT;
+
+
+        /* Rocket Leg */
+        public static Texture2D TEXTURE_ROBOT_ROCKETLEG_LEFTLEG_UP;
+        public static Texture2D TEXTURE_ROBOT_ROCKETLEG_RIGHTLEG_UP;
+
+        public static Texture2D TEXTURE_ROBOT_ROCKETLEG_LEFTLEG_DOWN;
+        public static Texture2D TEXTURE_ROBOT_ROCKETLEG_RIGHTLEG_DOWN;
+
+        public static Texture2D TEXTURE_ROBOT_ROCKETLEG_LEFTLEG_LEFT;
+
+        public static Texture2D TEXTURE_ROBOT_ROCKETLEG_RIGHTLEG_RIGHT;
+
+
+        /* Gun Leg */
+        public static Texture2D TEXTURE_ROBOT_GUNLEG_LEFTLEG_UP;
+        public static Texture2D TEXTURE_ROBOT_GUNLEG_RIGHTLEG_UP;
+
+        public static Texture2D TEXTURE_ROBOT_GUNLEG_LEFTLEG_DOWN;
+        public static Texture2D TEXTURE_ROBOT_GUNLEG_RIGHTLEG_DOWN;
+
+        public static Texture2D TEXTURE_ROBOT_GUNLEG_LEFTLEG_LEFT;
+
+        public static Texture2D TEXTURE_ROBOT_GUNLEG_RIGHTLEG_RIGHT;
+
+
+
 
         public static Texture2D texture_White;
         public static Texture2D texture_BG;
+
         public static SpriteFont font_Common;
 
         public static void LoadAll(ContentManager manager)
         {
-            //character sheet pieces
-            sheetHeads = new List<Texture2D>();
-            sheetHeads.Add(manager.Load<Texture2D>("Images//CharacterSheet//Head1"));
+            TEXTURE_HUMAN_UP_HEAD = manager.Load<Texture2D>("Images//Sprite//Human//Up_Head");
+            TEXTURE_HUMAN_UP_TORSO = manager.Load<Texture2D>("Images//Sprite//Human//Up_Torso");
+            TEXTURE_HUMAN_UP_LEFTARM = manager.Load<Texture2D>("Images//Sprite//Human//Up_LeftArm");
+            TEXTURE_HUMAN_UP_RIGHTARM = manager.Load<Texture2D>("Images//Sprite//Human//Up_RightArm");
+            TEXTURE_HUMAN_UP_LEFTLEG = manager.Load<Texture2D>("Images//Sprite//Human//Up_LeftLeg");
+            TEXTURE_HUMAN_UP_RIGHTLEG = manager.Load<Texture2D>("Images//Sprite//Human//Up_RightLeg");
 
-            sheetLeftArms = new List<Texture2D>();
-            sheetLeftArms.Add(manager.Load<Texture2D>("Images//CharacterSheet//LeftArm1"));
+            TEXTURE_HUMAN_DOWN_HEAD = manager.Load<Texture2D>("Images//Sprite//Human//Down_Head");
+            TEXTURE_HUMAN_DOWN_TORSO = manager.Load<Texture2D>("Images//Sprite//Human//Down_Torso");
+            TEXTURE_HUMAN_DOWN_LEFTARM = manager.Load<Texture2D>("Images//Sprite//Human//Down_LeftArm");
+            TEXTURE_HUMAN_DOWN_RIGHTARM = manager.Load<Texture2D>("Images//Sprite//Human//Down_RightArm");
+            TEXTURE_HUMAN_DOWN_LEFTLEG = manager.Load<Texture2D>("Images//Sprite//Human//Down_LeftLeg");
+            TEXTURE_HUMAN_DOWN_RIGHTLEG = manager.Load<Texture2D>("Images//Sprite//Human//Down_RightLeg");
 
-            sheetRightArms = new List<Texture2D>();
-            sheetRightArms.Add(manager.Load<Texture2D>("Images//CharacterSheet//RightArm1"));
+            TEXTURE_HUMAN_LEFT_HEAD = manager.Load<Texture2D>("Images//Sprite//Human//Left_Head");
+            TEXTURE_HUMAN_LEFT_TORSO = manager.Load<Texture2D>("Images//Sprite//Human//Left_Torso");
+            TEXTURE_HUMAN_LEFT_LEFTARM = manager.Load<Texture2D>("Images//Sprite//Human//Left_LeftArm");
+            TEXTURE_HUMAN_LEFT_LEFTLEG = manager.Load<Texture2D>("Images//Sprite//Human//Left_LeftLeg");
 
-            sheetLeftLegs = new List<Texture2D>();
-            sheetLeftLegs.Add(manager.Load<Texture2D>("Images//CharacterSheet//LeftLeg1"));
-
-            sheetRightLegs = new List<Texture2D>();
-            sheetRightLegs.Add(manager.Load<Texture2D>("Images//CharacterSheet//RightLeg1"));
-
-            sheetTorsos = new List<Texture2D>();
-            sheetTorsos.Add(manager.Load<Texture2D>("Images//CharacterSheet//Torso1"));
-
-            //sprite sprite pieces
-            spriteHeads = new List<Texture2D>();
-            spriteHeads.Add(manager.Load<Texture2D>("Images//Sprite//Human//head"));
-            spriteHeads.Add(manager.Load<Texture2D>("Images//Sprite//Robo//BiteHead"));
-
-            spriteLeftArms = new List<Texture2D>();
-            spriteLeftArms.Add(manager.Load<Texture2D>("Images//Sprite//Human//LeftArm"));
-            spriteLeftArms.Add(manager.Load<Texture2D>("Images//Sprite//Robo//LeftHookArm"));
-
-            spriteRightArms = new List<Texture2D>();
-            spriteRightArms.Add(manager.Load<Texture2D>("Images//Sprite//Human//RightArm"));
-            spriteRightArms.Add(manager.Load<Texture2D>("Images//Sprite//Robo//RightHookArm"));
-
-            spriteLeftLegs = new List<Texture2D>();
-            spriteLeftLegs.Add(manager.Load<Texture2D>("Images//Sprite//Human//LeftFoot"));
-            spriteLeftLegs.Add(manager.Load<Texture2D>("Images//Sprite//Robo//RoboLeftFoot"));
-
-            spriteRightLegs = new List<Texture2D>();
-            spriteRightLegs.Add(manager.Load<Texture2D>("Images//Sprite//Human//RightFoot"));
-            spriteRightLegs.Add(manager.Load<Texture2D>("Images//Sprite//Robo//RoboRightFoot"));
-
-            spriteTorsos = new List<Texture2D>();
-            spriteTorsos.Add(manager.Load<Texture2D>("Images//Sprite//Human//Torso"));
-            spriteTorsos.Add(manager.Load<Texture2D>("Images//Sprite//Robo//RoboTorso"));
+            TEXTURE_HUMAN_RIGHT_HEAD = manager.Load<Texture2D>("Images//Sprite//Human//Right_Head");
+            TEXTURE_HUMAN_RIGHT_TORSO = manager.Load<Texture2D>("Images//Sprite//Human//Right_Torso");
+            TEXTURE_HUMAN_RIGHT_RIGHTARM = manager.Load<Texture2D>("Images//Sprite//Human//Right_RightArm");
+            TEXTURE_HUMAN_RIGHT_RIGHTLEG = manager.Load<Texture2D>("Images//Sprite//Human//Right_RightLeg");
 
             //additional
             texture_White = manager.Load<Texture2D>("Images//white");
