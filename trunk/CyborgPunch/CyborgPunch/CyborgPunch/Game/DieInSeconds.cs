c61bcc1a815306af9da413501da17c66c6e55985
skarfgma@gmail.com <skarfgma@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2012-08-26 22:44:35 +0000

    picked up things work better

---
+++ trunk/CyborgPunch/CyborgPunch/CyborgPunch/Game/DieInSeconds.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CyborgPunch.Core;
+
+namespace CyborgPunch.Game
+{
+    class DieInSeconds : Component
+    {
+        float lifeSeconds;
+        public DieInSeconds(float seconds) : base()
+        {
+            lifeSeconds = seconds;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            lifeSeconds -= Time.deltaTime;
+            if (lifeSeconds <= 0f)
+            {
+                blob.Destroy();
+            }
+        }
+    }
+}
