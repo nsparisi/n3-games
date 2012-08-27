314bf4f6b396c66a12c75110e124468285d944c9
skarfgma@gmail.com <skarfgma@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2012-08-27 03:32:51 +0000

    better momentum

--- CyborgPunch/CyborgPunch/CyborgPunch/Game/DudeMovement.cs
+++ CyborgPunch/CyborgPunch/CyborgPunch/Game/DudeMovement.cs
@@ -13,9 +13,9 @@ namespace CyborgPunch.Game
         Humanoid body;
         //public float speed = 300;
 
-        float friction = .002f;
+        float friction = .0001f;
 
-        public Vector2 speed;
+        public Vector2 velocity;
 
         public DudeMovement(Humanoid body)
             : base()
@@ -27,37 +27,37 @@ namespace CyborgPunch.Game
         {
             base.Update();
 
-            float movementForce = 1400;
-            Vector2 force = Vector2.Zero;
+            float movementForce = 2000;
+            Vector2 acceleration = Vector2.Zero;
             float speedModifier = Math.Max((LegCount() / 2f), .25f);
             if (Keyboard.GetState().IsKeyDown(KeyBindings.MoveUp))
             {
                 //blob.transform.Translate(0, -modifiedSpeed * Time.deltaTime);
-                force.Y = -movementForce;
+                acceleration.Y = -movementForce;
                 body.SetFacing(Facing.Up);
             }
             else if (Keyboard.GetState().IsKeyDown(KeyBindings.MoveDown))
             {
                 //blob.transform.Translate(0, modifiedSpeed * Time.deltaTime);
-                force.Y = movementForce;
+                acceleration.Y = movementForce;
                body.SetFacing(Facing.Down);
             }
 
             if (Keyboard.GetState().IsKeyDown(KeyBindings.MoveLeft))
             {
                 //blob.transform.Translate(-modifiedSpeed * Time.deltaTime, 0);
-                force.X = -movementForce;
+                acceleration.X = -movementForce;
                 body.SetFacing(Facing.Left);
             }
             else if (Keyboard.GetState().IsKeyDown(KeyBindings.MoveRight))
             {
                 //blob.transform.Translate(modifiedSpeed * Time.deltaTime, 0);
-                force.X = movementForce;
+                acceleration.X = movementForce;
                 body.SetFacing(Facing.Right);
             }
-            speed += force*Time.deltaTime;
-            speed *= (float)Math.Pow(friction, Time.deltaTime);
-            blob.transform.Translate(speed*Time.deltaTime*speedModifier);
+            velocity += acceleration*Time.deltaTime;
+            blob.transform.Translate(velocity*Time.deltaTime*speedModifier);
+            velocity *= (float)Math.Pow(friction, Time.deltaTime);
         }
 
         public int LegCount()
