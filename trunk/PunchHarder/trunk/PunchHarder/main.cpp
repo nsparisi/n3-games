71e6d089faf5b52d90a508e9087714aa9b179d9b
NSParisi@gmail.com <NSParisi@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2012-12-28 17:52:48 +0000

    upgrading to SFML 2.0 - supposed to be better in general, better for porting to android, supposedly very stable

--- trunk/PunchHarder/trunk/PunchHarder/main.cpp
+++ trunk/PunchHarder/trunk/PunchHarder/main.cpp
@@ -1,13 +1,104 @@
-
-////////////////////////////////////////////////////////////
-// Headers
-////////////////////////////////////////////////////////////
 #include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
-#include <iostream>
+#include <SFML/OpenGL.hpp>
+
+void drawCube();
+
+int main()
+{
+    // create the window
+    sf::Window window(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, sf::ContextSettings(32));
+    window.setVerticalSyncEnabled(true);
+
+    // load resources, initialize the OpenGL states, ...
+
+    //clock does time calculations
+    sf::Clock clock; // starts the clock
+    sf::Clock deltaClock;
+
+    //set color and depth clear value
+    glClearDepth(1.0f);
+    glClearColor(0.0f,0.0f,0.0f,0.0f);
+
+    //enable z-buffer read and write
+    glEnable(GL_DEPTH_TEST);
+    glDepthMask(GL_TRUE);
+
+    //setup a perspective projection
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(90.0f,1.0f,1.0f,1000.0f);
 
 
-void draw_cube()
+    // run the main loop
+    bool running = true;
+    while (running)
+    {
+        // while there are pending events...
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // check the type of the event...
+            switch (event.type)
+            {
+                // window closed
+                case sf::Event::Closed:
+                    running = false;
+                    break;
+
+                // key pressed
+                case sf::Event::KeyPressed:
+                    if(event.key.code == sf::Keyboard::Escape)
+                    {
+                        running = false;
+                    }
+                    break;
+
+                // resize event
+                case sf::Event::Resized:
+                    glViewport(0, 0, event.size.width, event.size.height);
+                    break;
+
+                // we don't process other types of events
+                default:
+                    break;
+            }
+        }
+
+        // clear the buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // draw some cubes
+        float deltaTime = deltaClock.restart().asSeconds();
+        float elapsedTime = clock.getElapsedTime().asSeconds();
+
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        glPushMatrix();
+            glTranslatef(100.f, -20.f, -600.f);
+            glRotatef(elapsedTime * 30, 0.f, 1.f, 0.f);
+            glRotatef(elapsedTime * 90, 0.f, 0.f, 1.f);
+            drawCube();
+        glPopMatrix();
+
+        glPushMatrix();
+            glTranslatef(-50.f, 50.f, -400.f);
+            glRotatef(elapsedTime * 50, 1.f, 0.f, 0.f);
+            glRotatef(elapsedTime * 30, 0.f, 1.f, 0.f);
+            drawCube();
+        glPopMatrix();
+
+        // end the current frame (internally swaps the front and back buffers)
+        window.display();
+    }
+
+    // release resources...
+
+    return 0;
+}
+
+
+void drawCube()
 {
     // Draw a cube
     glBegin(GL_QUADS);
@@ -42,83 +133,4 @@ void draw_cube()
         glVertex3f( 50.f, 50.f, -50.f);
         glVertex3f( 50.f, 50.f,  50.f);
     glEnd();
-}
-
-////////////////////////////////////////////////////////////
-/// Entry point of application
-///
-/// \return Application exit code
-///
-////////////////////////////////////////////////////////////
-int main()
-{
-    // Create main window
-    sf::RenderWindow App(sf::VideoMode(800, 600), "Punch Harder");
-
-    //clock does time calculations
-    sf::Clock Clock;
-
-    //set color and depth clear value
-    glClearDepth(1.0f);
-    glClearColor(0.0f,0.0f,0.0f,0.0f);
-
-    //enable z-buffer read and write
-    glEnable(GL_DEPTH_TEST);
-    glDepthMask(GL_TRUE);
-
-    //setup a perspective projection
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(90.0f,1.0f,1.0f,500.0f);
-
-    // Start game loop
-    while (App.IsOpened())
-    {
-        // Process events
-        sf::Event Event;
-        while (App.GetEvent(Event))
-        {
-            // Close window : exit
-            if (Event.Type == sf::Event::Closed)
-                App.Close();
-
-            // Escape key : exit
-            if ((Event.Type == sf::Event::KeyPressed) && (Event.Key.Code == sf::Key::Escape))
-                App.Close();
-
-            //when resized, recalc drawing viewport
-            if (Event.Type == sf::Event::Resized)
-                glViewport(0, 0, Event.Size.Width, Event.Size.Height);
-        }
-
-        // Set the active window before using OpenGL commands
-        // It's useless here because active window is always the same,
-        // but don't forget it if you use multiple windows or controls
-        App.SetActive();
-
-        // Clear color and depth buffer
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // Apply some transformations
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glPushMatrix();
-            glTranslatef(100.f, -20.f, -300.f);
-            glRotatef(Clock.GetElapsedTime() * 30, 0.f, 1.f, 0.f);
-            glRotatef(Clock.GetElapsedTime() * 90, 0.f, 0.f, 1.f);
-            draw_cube();
-        glPopMatrix();
-
-        glPushMatrix();
-            glTranslatef(-50.f, 50.f, -400.f);
-            glRotatef(Clock.GetElapsedTime() * 50, 1.f, 0.f, 0.f);
-            glRotatef(Clock.GetElapsedTime() * 30, 0.f, 1.f, 0.f);
-            draw_cube();
-        glPopMatrix();
-
-        // Finally, display rendered frame on screen
-        App.Display();
-    }
-
-    return EXIT_SUCCESS;
 }
