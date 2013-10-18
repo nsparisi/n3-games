f3aa765db4612a702cc9c63917c823078d94e2ca
skarfgma@gmail.com <skarfgma@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2013-10-18 05:04:48 +0000

    Starting the sprite class

---
+++ PunchLine/Unity/Assets/Scripts/gfx/Sprite.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+
+public class Sprite : MonoBehaviour {
+	
+	public Color color;
+	
+	public Vector2 size;
+	public Vector2 Size {
+		get { 
+			return size;
+		} 
+		set {
+			size = value;
+			vertices = new Vector3[4];
+			vertices[0] = new Vector3(0, 0f, 0f);
+			vertices[1] = new Vector3(value.x, 0f, 0f);
+			vertices[2] = new Vector3(0f, value.y, 0f);
+			vertices[3] = new Vector3(value.x, value.y, 0f);
+			mesh.vertices = vertices;
+		}
+	}
+	
+	private MeshFilter filter;
+//	private MeshRenderer renderer;
+	private Mesh mesh;
+	
+	private Vector3[] vertices;
+	private Vector2[] uvs = {new Vector2(0f,0f), new Vector2(1f,0f), new Vector2(0f,1f), new Vector2(1f,1f)};
+	private int[] triangles = {0,1,2,1,3,0};
+	
+	void Awake()
+	{
+		mesh = new Mesh();
+		filter = this.GetComponent<MeshFilter>();
+		filter.mesh = mesh;
+//		renderer = this.GetComponent<MeshRenderer>();
+		
+		Size = size;
+		mesh.vertices = vertices;
+		mesh.uv = uvs;
+		mesh.triangles = triangles;
+	}
+	
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		
+	}
+}
---
+++ PunchLine/Unity/Assets/Scripts/levelGeometry/LevelBlock.cs
@@ -0,0 +1,16 @@
+﻿//this takes
+
+using UnityEngine;
+using System.Collections;
+
+public class LevelBlock : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
