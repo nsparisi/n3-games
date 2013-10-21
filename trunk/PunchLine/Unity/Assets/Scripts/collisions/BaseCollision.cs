c865a3bd292c46d4c621a0b6f5908e2f160ae018
nsparisi@gmail.com <nsparisi@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2013-10-21 01:38:50 +0000

    sword swinging

---
+++ trunk/PunchLine/Unity/Assets/Scripts/collisions/BaseCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class BaseCollision : MonoBehaviour 
+{
+	void Awake()
+	{
+		this.collider.isTrigger = true;
+	}
+	
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log(string.Format("{0} touched other: {1}", this.collider.name, other.name));	
+	}
+}
