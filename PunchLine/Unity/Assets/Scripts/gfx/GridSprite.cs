35ee5c76fdba697fac0792950e595ef28731c7df
skarfgma@gmail.com <skarfgma@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2013-10-25 05:21:12 +0000

    sprites that you can set by frame number

---
+++ PunchLine/Unity/Assets/Scripts/gfx/GridSprite.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+
+public class GridSprite : Sprite {
+	
+	public int framesWide = 1;
+	public int FramesWide {
+		get {
+			return framesWide;
+		}
+		
+		set {
+			framesWide = 0;
+		}
+	}
+	
+	public int framesHigh = 1;
+	public int FramesHigh {
+		get {
+			return framesHigh;
+		}
+		set {
+			framesHigh = value;
+		}
+	}
+
+	public int currentFrame;
+	public int CurrentFrame {
+		get {
+			return currentFrame;
+		}
+		
+		set {
+			currentFrame = value;
+			
+			if (currentFrame >= (FramesHigh*FramesWide)-1)
+			{
+				currentFrame = 0;
+			}
+			
+			int spriteRow = currentFrame / FramesWide;
+			int spriteColumn = currentFrame % FramesWide;
+			
+			float frameWidth = 1f / FramesWide;
+			float frameHeight = 1f / FramesHigh;
+			
+			float left = spriteColumn*frameWidth;
+			float bottom = spriteRow*frameHeight;
+			float right = (spriteColumn+1)*frameWidth;
+			float top = (spriteRow+1)*frameHeight;
+			UV = new Vector4(left, bottom, right, top);
+		}
+	}
+	
+	public new void Awake()
+	{
+		base.Awake();
+		CurrentFrame = currentFrame;
+	}
+}
--- PunchLine/Unity/Assets/Scripts/gfx/Sprite.cs
+++ PunchLine/Unity/Assets/Scripts/gfx/Sprite.cs
@@ -1,57 +1,79 @@
 ﻿using UnityEngine;
 using System.Collections;
+
 [ExecuteInEditMode]
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
 
-public class Sprite : MonoBehaviour {
+public class Sprite : MonoBehaviour
+{
+	public Color color = new Color (1f, 1f, 1f, 1f);
+	public Color Color {
+		get { 
+			return color; 
+		}
+		set {
+			color = value;
+			colors[0] = color;
+			colors[1] = color;
+			colors[2] = color;
+			colors[3] = color;
+			mesh.colors = colors;
+		}
+	}
 	
-	public Color color = new Color(1f, 1f, 1f, 1f);
-	
-	public Vector2 size;
+	public Vector2 size = new Vector2(1f, 1f);
 	public Vector2 Size {
 		get { 
 			return size;
 		} 
 		set {
 			size = value;
-			vertices = new Vector3[4];
-			vertices[0] = new Vector3(0, 0f, 0f);
-			vertices[1] = new Vector3(value.x, 0f, 0f);
-			vertices[2] = new Vector3(0f, value.y, 0f);
-			vertices[3] = new Vector3(value.x, value.y, 0f);
+			vertices [0] = new Vector3 (0, 0f, 0f);
+			vertices [1] = new Vector3 (value.x, 0f, 0f);
+			vertices [2] = new Vector3 (0f, value.y, 0f);
+			vertices [3] = new Vector3 (value.x, value.y, 0f);
 			mesh.vertices = vertices;
 		}
 	}
 	
-	private MeshFilter filter;
-//	private MeshRenderer renderer;
-	private Mesh mesh;
+	/// <summary>
+	/// Use this. Order is left, down, right, up (like two sets of vector 2s).
+	/// </summary>
+	public Vector4 uv = new Vector4(0f,0f,1f,1f);
+	public Vector4 UV {
+		get {
+			return uv;
+		}
+		set {
+			uv = value;
+			uvs[0] = new Vector2(uv.x, uv.y);
+			uvs[1] = new Vector2(uv.z, uv.y);
+			uvs[2] = new Vector2(uv.x, uv.w);
+			uvs[3] = new Vector2(uv.z, uv.w);
+			mesh.uv = uvs;
+		}
+	}
 	
-	private Vector3[] vertices;
-	private Vector2[] uvs = {new Vector2(0f,0f), new Vector2(1f,0f), new Vector2(0f,1f), new Vector2(1f,1f)};
+	private MeshFilter filter;
+	private Mesh mesh;
+	private Vector3[] vertices = new Vector3[4];
+	/// <summary>
+	/// Don't ever use this. Lower left, lower right, upper left, upper right
+	/// </summary>
+	private Vector2[] uvs = {new Vector2 (0f, 0f), new Vector2 (1f, 0f), new Vector2 (0f, 1f), new Vector2 (1f, 1f)};
+	private Color[] colors = { Color.white, Color.white, Color.white, Color.white };
 	private int[] triangles = {2,1,0,2,3,1};
 	
-	void Awake()
+	public void Awake()
 	{
-		mesh = new Mesh();
-		filter = this.GetComponent<MeshFilter>();
+		mesh = new Mesh ();
+		filter = this.GetComponent<MeshFilter> ();
 		filter.mesh = mesh;
-//		renderer = this.GetComponent<MeshRenderer>();
 		
 		Size = size;
-		mesh.vertices = vertices;
-		mesh.uv = uvs;
+		UV = uv;
+		Color = color;
 		mesh.triangles = triangles;
-	}
-	
-	void Start()
-	{
-		
-	}
-	
-	void Update()
-	{
-		
 	}
 }
