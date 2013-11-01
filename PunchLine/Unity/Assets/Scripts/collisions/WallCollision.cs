a2b8d1a3cba34c083da4ab6f3a815e471b3ac2fe
nsparisi@gmail.com <nsparisi@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2013-11-01 04:04:35 +0000

    player uses charactercontroller + collision sliding

--- PunchLine/Unity/Assets/Scripts/collisions/WallCollision.cs
+++ PunchLine/Unity/Assets/Scripts/collisions/WallCollision.cs
@@ -6,13 +6,5 @@ public class WallCollision : BaseCollision
 {
 	void OnTriggerStay (Collider other)
 	{
-		// Debug.Log(string.Format("Entity {0} touched other: {1}", this.collider.name, other.name));	
-		
-		BaseCollision collision = other.GetComponent<BaseCollision>();
-		if(collision is EntityCollision)
-		{
-			EntityCollision entityCollision = (EntityCollision)collision;
-			entityCollision.entity.TouchedByWall(this.collider);
-		} 
 	}
 }
