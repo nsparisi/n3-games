8e3fbf3c54679db235b6c02a0b0f437d329ec466
nsparisi@gmail.com <nsparisi@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2013-11-08 06:01:50 +0000

    enemy has weapon shield

--- trunk/PunchLine/Unity/Assets/Scripts/player/AttackSegment.cs
+++ trunk/PunchLine/Unity/Assets/Scripts/player/AttackSegment.cs
@@ -6,20 +6,26 @@ public class AttackSegment : MonoBehaviour
 	public int startFrame;
 	public int frameCount;
 	
+	private Vector3 onScreen;
+	private static Vector3 offScreen = new Vector3(-100, -100, 0);
+	
 	void Awake()
 	{
 		this.GetComponent<Collider>().enabled = true;
+		onScreen = this.transform.localPosition;
 	}
 	
-	public void Enable()
+	public void DoEnable()
 	{
 		//this.GetComponent<Collider>().enabled = true;
 		this.GetComponent<Collider>().gameObject.SetActive(true);
+		//this.transform.localPosition = onScreen;
 	}
 	
-	public void Disable()
+	public void DoDisable()
 	{
 		//this.GetComponent<Collider>().enabled = false;
 		this.GetComponent<Collider>().gameObject.SetActive(false);
+		//this.transform.position = offScreen;
 	}
 }
