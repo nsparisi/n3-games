1fb13c7805b9d26f00f0211c451e9bf919c13246
nsparisi@gmail.com <nsparisi@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2013-12-13 05:56:14 +0000

    snap to grid semi-fix

--- trunk/PunchLine/Unity/Assets/Scripts/environment/SnapToGrid.cs
+++ trunk/PunchLine/Unity/Assets/Scripts/environment/SnapToGrid.cs
@@ -1,13 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
 [ExecuteInEditMode]
 public class SnapToGrid : MonoBehaviour {
 	const float GridSize = 64f;
 	const float OneOverGridSize = 1f / GridSize;
+
+	void OnEnable()
+	{
+		EditorApplication.update += SnapUpdate;
+	}
+
+	void OnDisable()
+	{
+		EditorApplication.update -= SnapUpdate;
+	}
 	
 	// Update is called once per frame
-	void Update () {
+	void SnapUpdate () {
+		if(Selection.instanceIDs.Length > 1)
+		{
+			return;
+		}
+
 		float x = Mathf.Round(this.transform.position.x * OneOverGridSize) * GridSize;
 		float y = Mathf.Round(this.transform.position.y * OneOverGridSize) * GridSize;
 		float z = this.transform.position.z;
