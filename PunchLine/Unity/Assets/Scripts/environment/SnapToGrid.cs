b3bd9300b835714f456e7c6c1d00b5d4acc77d28
nsparisi@gmail.com <nsparisi@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2013-12-13 04:54:58 +0000

    AlignZWithY - for mize en scene remove sprite from player parent object + update animations

--- PunchLine/Unity/Assets/Scripts/environment/SnapToGrid.cs
+++ PunchLine/Unity/Assets/Scripts/environment/SnapToGrid.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 [ExecuteInEditMode]
 public class SnapToGrid : MonoBehaviour {
-	const int GridSize = 64;
+	const float GridSize = 64f;
 	const float OneOverGridSize = 1f / GridSize;
 	
 	// Update is called once per frame
 	void Update () {
-		float x = Mathf.RoundToInt(this.transform.position.x * OneOverGridSize) * GridSize;
-		float y = Mathf.RoundToInt(this.transform.position.y * OneOverGridSize) * GridSize;
+		float x = Mathf.Round(this.transform.position.x * OneOverGridSize) * GridSize;
+		float y = Mathf.Round(this.transform.position.y * OneOverGridSize) * GridSize;
 		float z = this.transform.position.z;
 
 		this.transform.position = new Vector3(x, y, z);
