0c400b60d2c7d675862104a7a6fa738eed3e7dff
nsparisi@gmail.com <nsparisi@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2013-12-13 04:54:58 +0000

    AlignZWithY - for mize en scene remove sprite from player parent object + update animations

---
+++ trunk/PunchLine/Unity/Assets/Scripts/environment/AlignZWithY.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class AlignZWithY : MonoBehaviour {
+
+	const float zStep = 0.01f;
+	const float zOrigin = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+		float x = this.transform.position.x;
+		float y = this.transform.position.y;
+		float z = y * zStep + zOrigin;
+
+		this.transform.position = new Vector3(x, y, z);
+	}
+}
