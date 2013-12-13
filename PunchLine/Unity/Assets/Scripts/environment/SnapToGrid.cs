285759d9a2973f68cd83786babbd1be75878c188
nsparisi@gmail.com <nsparisi@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2013-12-13 04:29:03 +0000

    snap to grid

---
+++ PunchLine/Unity/Assets/Scripts/environment/SnapToGrid.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SnapToGrid : MonoBehaviour {
+	const int GridSize = 64;
+	const float OneOverGridSize = 1f / GridSize;
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Mathf.RoundToInt(this.transform.position.x * OneOverGridSize) * GridSize;
+		float y = Mathf.RoundToInt(this.transform.position.y * OneOverGridSize) * GridSize;
+		float z = this.transform.position.z;
+
+		this.transform.position = new Vector3(x, y, z);
+	}
+}
