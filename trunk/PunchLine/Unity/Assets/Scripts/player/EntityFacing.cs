e4f3d425d068f16031aa21c766cf7ccebc860381
skarfgma@gmail.com <skarfgma@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2014-06-15 21:24:53 +0000

    octorok

--- trunk/PunchLine/Unity/Assets/Scripts/player/EntityFacing.cs
+++ trunk/PunchLine/Unity/Assets/Scripts/player/EntityFacing.cs
@@ -49,4 +49,95 @@ public class Facing
 			return new Vector3(0,0);
 		}
 	}
+
+	public static EntityFacing RandomFacingThatIsntThisOne(EntityFacing thisOne)
+	{
+		int rand = Random.Range(0,3);
+		EntityFacing[] choiceArray = new EntityFacing[3];
+		int index = 0;
+		if (thisOne != EntityFacing.Up)
+		{
+			choiceArray[index++] = EntityFacing.Up;
+		}
+
+		if (thisOne != EntityFacing.Down)
+		{
+			choiceArray[index++] = EntityFacing.Down;
+		}
+
+		if (thisOne != EntityFacing.Left)
+		{
+			choiceArray[index++] = EntityFacing.Left;
+		}
+
+		if(thisOne != EntityFacing.Right)
+		{
+			choiceArray[index++] = EntityFacing.Right;
+		}
+
+		return choiceArray[rand];
+	}
+
+	public static EntityFacing RotateClockwise(EntityFacing facing)
+	{
+		switch(facing)
+		{
+		case EntityFacing.Up:
+			return EntityFacing.Right;
+		case EntityFacing.Down:
+			return EntityFacing.Left;
+		case EntityFacing.Left:
+			return EntityFacing.Up;
+		case EntityFacing.Right:
+			return EntityFacing.Down;
+		default:
+			return EntityFacing.Up;
+		}
+	}
+
+	public static EntityFacing RotateCounterclockwise(EntityFacing facing)
+	{
+		switch(facing)
+		{
+		case EntityFacing.Up:
+			return EntityFacing.Left;
+		case EntityFacing.Down:
+			return EntityFacing.Right;
+		case EntityFacing.Left:
+			return EntityFacing.Down;
+		case EntityFacing.Right:
+			return EntityFacing.Up;
+		default:
+			return EntityFacing.Up;
+		}
+	}
+
+	public static EntityFacing OppositeFacing(EntityFacing facing)
+	{
+		switch(facing)
+		{
+		case EntityFacing.Up:
+			return EntityFacing.Down;
+		case EntityFacing.Down:
+			return EntityFacing.Up;
+		case EntityFacing.Left:
+			return EntityFacing.Right;
+		case EntityFacing.Right:
+			return EntityFacing.Left;
+		default:
+			return EntityFacing.Up;
+		}
+		
+	}
+//	switch(facing)
+//	{
+//		case EntityFacing.Up:
+//		break;
+//		case EntityFacing.Down:
+//		break;
+//		case EntityFacing.Left:
+//		break;
+//		case EntityFacing.Right:
+//		break;
+//	}
 }
