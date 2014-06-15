4a22adbfa38d3b0b2f35b230f71a4c21673d17a3
nsparisi@gmail.com <nsparisi@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2014-06-15 19:37:44 +0000

    player pots part 1

---
+++ PunchLine/Unity/Assets/Scripts/player/PotSensor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PotSensor : MonoBehaviour {
+	
+	public Pot TouchingPot { get; private set; }
+	
+	bool waitAFrame;
+	
+	void FixedUpdate()
+	{
+		if (!waitAFrame)
+		{
+			TouchingPot = null;
+		} 
+		waitAFrame = false;
+	}
+	
+	void OnTriggerStay (Collider other)
+	{
+		waitAFrame = true;
+
+		TouchingPot = other.GetComponent<Pot>();
+	}
+}
