9746fb7c5ef53a4bf4663c19547fde4dc79bf1cf
skarfgma@gmail.com <skarfgma@gmail.com@8fb859cd-6bf1-8cb3-9852-507fe0176626>
2014-06-16 03:10:47 +0000

    hamster runs

---
+++ PunchLine/Unity/Assets/Scripts/enemy/MaskedHamsterEnemy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaskedHamsterEnemy : Enemy {
+	public float acceleration;
+	public float maxSpeed;
+	public Vector3 velocity;
+
+	public float skidValue;
+
+	protected override void RunAI ()
+	{
+		//accelerate towards player
+		Vector3 direction = (Player.Instance.transform.position - transform.position).normalized;
+		Vector3 accelerationVector = direction*acceleration;
+		velocity += accelerationVector*Time.fixedDeltaTime;
+
+		float dotProduct = Vector3.Dot(direction, velocity);
+		//velocity moves away from destination, skid
+		if (dotProduct < 0)
+		{
+			velocity -= accelerationVector*Time.fixedDeltaTime;
+			velocity = velocity*Mathf.Pow(skidValue, Time.fixedDeltaTime);
+		}
+		else
+		{
+			if (velocity.magnitude > maxSpeed)
+			{
+				velocity = velocity.normalized*maxSpeed;
+			}
+		}
+
+		base.MoveWithSliding(velocity*Time.fixedDeltaTime);
+	}
+}
